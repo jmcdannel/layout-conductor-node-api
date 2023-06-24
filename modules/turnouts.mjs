497da101fb7bdad4eb7ceff7ada31d29e9680182
Josh McDannel <mcdannel@gmail.com>
2023-06-24 15:07:37 -0600

    update config, add turnout save method

--- modules/turnouts.mjs
+++ modules/turnouts.mjs
@@ -1,8 +1,23 @@
 
-import layout from './layout.mjs';
+import { module, path as modulePath }  from './layout.mjs';
+import { writeFile } from 'fs';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const TURNOUTS = 'turnouts';
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const save = turnouts => {
+  const modPath = path.resolve(__dirname, modulePath(TURNOUTS));
+  writeFile(modPath, JSON.stringify(turnouts), function(err) {
+    if (err) throw err;
+    console.log('turnouts.save complete');
+  });
+}
 
 export const get = () => {
-  return layout.module('turnouts');
+  return module(TURNOUTS);
 }
 
 export default {
